21f1eb5b7ea6a121350718f090de930e085a030c
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2018-02-04 21:18:18 -0500

    ADDED API ENDPOINTS TO CHECK SYSTEM AND DATABASE STATUS.

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const http = require("http");
+const dotenv = require("dotenv").config();
 const express = require("express");
 const events = require("events");
+const serviceAccountKey = require("./service-account-key.json")
 const app = express();
 const server = http.createServer(app);
 const baseConfig = require("./server/config");
+const admin = require("firebase-admin");
 
 baseConfig.setLogAndExceptionConfig(app);
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccountKey || process.env.serviceAccountKey),
+	databaseURL: process.env.DATABASE_URL
+});
 
-app.get("/api/v1/test", (request, response) => {
+const db = admin.database();
+const auth = admin.auth();
+
+app.get("/api/v1/test", (request, response)=> {
 	response.json({"msg": "System operating within normal parameters."});
 });
 
-server.listen(process.env.PORT || 8080);
+app.get("/api/v1/dbtest", (request, response)=> {
+	db.ref("test").once("value")
+	.then((snapshot)=> snapshot.val())
+	.then(data => response.json(data))
+	.catch(onError);
+})
 
+function onError(error) {
+	console.error(error);
+	return error;
+}
+
+
+
+server.listen(process.env.PORT || 8080);
 console.log("Express server listening on port %d in %s mode",
 server.address().port, app.settings.env)
