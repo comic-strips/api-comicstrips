c561064b9ce88ba54d2c2f18908ba1a212ab544a
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2018-02-06 16:35:29 -0500

    CREATED MOCK FOLDER FOR BOOKING MODULE.

--- api/booking/index.js
+++ api/booking/index.js
@@ -2,34 +2,6 @@
 function bookingModule($imports) {
 	const {app, utils} = $imports;
 	const eventEmitter = utils.eventEmitter;
-
-	/*const booking =  { 
-		bookingData: { 
-			recipientId: 'NOT/ISSUED',
-		        customerId: 'E8sFGm7u9tSNTGD9e25DWhtDAmx1',
-		        talentId: 'NOT/ISSUED',
-		        accountManagerId: '3WDebwKtgmSA25CrYRtkA6FMIxD2',
-		        transactionId: null,
-		        bookingCreationDate: 1517889815160,
-		        status: 'PENDING',
-		        paymentStatus: 'HELD',
-		        customizations: 'Make it extra special.',
-		        products: ["Array"],
-		        time: '15:00',
-		        date: 123456780000,
-		        location: ["Object"],
-		        notes: 'To be or not be. That is the question.' 
-		},
-	     	recipientData: { 
-	     		customerId: 'E8sFGm7u9tSNTGD9e25DWhtDAmx1',
-	        	firstName: 'Jackie',
-	        	lastName: 'Robinson',
-	        	email: 'jackie@dodgers.la',
-	        	phoneNumber: '1-800-SAFE-AUTO',
-	        	bookings: ["Array"] 
-	        }
-     	};*/
-
      	const bookingId = "-L4dH55siqby7ecSnHFq";
 	
 	app.post("/api/v1/bookings/create", (request, response)=> {
@@ -48,8 +20,6 @@ function bookingModule($imports) {
 			.then(onBookingCreated)
 			.catch(onError);
 		}).catch(onError); 
-
-		//onBookingCreated({bookingId, booking});
 	});
 
 	function onBookingCreated({bookingId, booking}) {
