dd426fd94c93781b707610949861cce4d85ce175
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2018-02-18 20:58:59 -0500

    FIXED ISSUE NO. 31 on '/api/v1/booking-offer' route. Created .json config file for sms module. Created './sms/templates/' folder to house sms message templates.

--- api/sms/index.js
+++ api/sms/index.js
@@ -1,78 +1,78 @@
 function SMSModule($imports) {
-        const twilio = require("twilio");
-        const config = require("./message-templates.json");
-        const accountSID = process.env.TWILIO_SID;
-        const authToken = process.env.TWILIO_AUTH_TOKEN;
-        const twilioNumber = process.env.TWILIO_NUMBER;
-        const client = new twilio(accountSID, authToken);
-        const { app, utils } = $imports;
-        const { eventEmitter } = utils;
-        const ejs = require("ejs");
+	const twilio = require("twilio");
+	const message = require("./templates/talent/booking-offer-sms.json");
+	const accountSID = process.env.TWILIO_SID;
+	const authToken = process.env.TWILIO_AUTH_TOKEN;
+	const twilioNumber = process.env.TWILIO_NUMBER;
+	const client = new twilio(accountSID, authToken);
+	const { app, utils } = $imports;
+	const { eventEmitter } = utils;
+	const ejs = require("ejs");
 
-        eventEmitter.on("sms:contactListCreated", onContactListCreated);
+	eventEmitter.on("sms:contactListCreated", onContactListCreated);
 
-        app.post("/api/v1/booking-offer", (request, response)=> {
-                /*check to see if booking offer is already accepted.*/
-                if (!request.body.Body.includes("ACCEPT")) {
-                        response.json({
-                                code: "sms/offer-declined",
-                                message: "The booking offer was declined."
-                        });
-                        return;
-                }
+	app.post("/api/v1/booking-offer", (request, response)=> {
+		/*check to see if booking offer is already accepted.*/
+		if (!request.body.Body.includes("ACCEPT")) {
+			response.json({
+				code: "sms/offer-declined",
+				message: "The booking offer was declined."
+			});
+			return;
+		}
 
-                parseOfferResponse({
-                        messsageBody: request.body.Body,
-                        from: request.body.From
-                })
-                .then((booking)=> {
-                        response.json({ bookingId: booking.id });
-                        return booking;
-                })
-                .then((booking)=> {
-                       return eventEmitter.emit("mailer:bookingConfirmed",booking);
-                })
-                .catch(onError);
-        });
+		parseOfferResponse({
+			messsageBody: request.body.Body,
+			from: request.body.From
+		})
+		.then((booking)=> {
+			response.json({ bookingId: booking.id });
+			return booking;
+		})
+		.then((booking)=> {
+		       return eventEmitter.emit("mailer:bookingConfirmed",booking);
+		})
+		.catch(onError);
+	});
 
-        function buildSMSMessage(template, data) {
-                return ejs.render(template, { data });
-        };
+	function buildSMSMessage(template, data) {
+		return ejs.render(template, { data });
+	};
 
-        function onContact(booking, contact) {
-                client.messages.create(Object.assign({}, {
-                        body: buildSMSMessage(config.bookingOfferOutgoingMsg, {
-                                booking,
-                                contact
-                        }),
-                        to: contact.phoneNumber,
-                        from: twilioNumber
-                })).catch(onError);
-        };
+	function onContact(booking, contact) {
+		client.messages.create(Object.assign({}, {
+			body: buildSMSMessage(message.body, {
+				booking,
+				contact
+			}),
+			to: contact.phoneNumber,
+			from: twilioNumber
+		})).catch(onError);
+	};
 
-        function onContactListCreated({booking, contactList}) {
-                contactList.forEach(onContact.bind(null, booking));
-                return booking;
-        };
+	function onContactListCreated({booking, contactList}) {
+		contactList.forEach(onContact.bind(null, booking));
+		return booking;
+	};
 
-        function parseOfferResponse({ messsageBody, from }) {
-                const [, bookingRefNumber] = messsageBody.split(" ");
-                const talentPhoneNumber = from;
+	function parseOfferResponse({ messsageBody, from }) {
+		const [, bookingRefNumber] = messsageBody.split(" ");
+		const talentPhoneNumber = from;
 
-                return eventEmitter.emit("db/booking:finalizeBooking", {
-                        bookingRefNumber: parseInt(bookingRefNumber, 0),
-                        talentPhoneNumber
-                });
-        };
+		return eventEmitter.emit("db/booking:finalizeBooking", {
+			bookingRefNumber: parseInt(bookingRefNumber, 0),
+			talentPhoneNumber
+		});
+	};
 
-        function onError(error) {
-                console.error(error);
-                return {
-                        code: "sms:error",
-                        msg: error.message,
-                        stack: error.stack.split("\n")
-                };
-        };
+	function onError(error) {
+		console.error(error);
+		return {
+			code: "sms:error",
+			msg: error.message,
+			stack: error.stack.split("\n")
+		};
+	};
 }
 
 module.exports = SMSModule;
