5f7ba145287088da519a080b5fd35bec8501814d
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2018-08-13 10:59:11 -0400

    CREATED '/issues' ENDPOINT FOR ACCESSING ERROR EVENTS.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ module.exports = {
   systemAPI: require("./api/system"),
   customersAPI: require("./api/customers"), 
   bookingsAPI: require("./api/bookings"), 
+  issuesAPI: require("./api/issues"),
   accountsAPI: require("./api/accounts"),
   recipientsAPI: require("./api/recipients"),
   offersAPI: require("./api/offers"),
